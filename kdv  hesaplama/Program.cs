6eb17fbd46ec491c4d4d7cecb2b2a64cdab0459f
umut-turk <113532432+umut-turk@users.noreply.github.com>
2023-07-11 22:24:52 +0300

    kdvhesaplama

---
+++ kdv  hesaplama/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kdv__hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kdv  oranın   lütfen  0,8  veya 0,1 veya 0,18  diye  yazınız");
+            Console.WriteLine("ürün fiyatı");
+            string girlenfıyat = Console.ReadLine();
+            float fiyat=Convert.ToSingle(girlenfıyat);
+
+
+
+            Console.WriteLine("kdv oranı");
+            string gkdv = Console.ReadLine();
+            float kdv = Convert.ToSingle(gkdv);
+            float sonuc1 = fiyat * kdv / 100;
+
+            Console.WriteLine("ürün kdv   dahil  olamndkı   fiyatı"+" "+ sonuc1 +" " +"tldir");
+           
+
+            Console.ReadLine();
+        }
+    }
+}
